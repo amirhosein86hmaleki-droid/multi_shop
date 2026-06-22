8547ae5364effa3ab338c6e76a859c895e067a2f
Amir hm <amirhosein86hmaleki@gmail.com>
2026-06-21 20:29:30 -0700

    feat: add to cart

--- cart/views.py
+++ cart/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from product.models import Product
+from .cart_module import Cart
 
 class CartDetailView(View):
     def get(self, request):
-        return render(request, 'cart/cart_detail.html', {})
+        cart = Cart(request)
+        return render(request, 'cart/cart_detail.html', {'cart':cart})
     
 
 class CartAddView(View):
     def post(self, request,pk):
         product = get_object_or_404(Product, id=pk)
-        size, color, quantity = request.POST.get('size'), request.POST.get('color'),  request.POST.get('quantity')
+        size, color, quantity = request.POST.get('size', '_'), request.POST.get('color', '_'),  request.POST.get('quantity')
+        cart = Cart(request)
+        cart.add(product, quantity, color, size)
         return redirect('cart:cart_detail')
+
+class CartDeleteView(View):
+    def get(self, request, id):
+        cart = Cart(request)
+        cart.delete(id)
+        return redirect('cart:cart_detail')
+
